8d9778ea65ee6947024f2accc135dbe503c78191
Helen Killeen <hjkilleen@ucdavis.edu>
2020-02-27 11:16:01 -0800

    Add histGrid function which pairs with plotHist

--- scripts/functions/length_frequency.R
+++ scripts/functions/length_frequency.R
@@ -2,7 +2,15 @@
 plotHist <- function(j) {
   ggplot(j, aes(x = length, color = sex)) + 
     geom_histogram(fill = "white", alpha = 0.5, position = "identity") +
-    ggtitle(paste(j$station[1], j$species[1], sep = " ")) + 
+    ggtitle(paste(j$station[1], j$species[1], j$year, sep = " ")) + 
     xlim(5, 35) +
     theme(plot.title = element_text(hjust = 0.5))
+}
+
+#Plot a grid of histograms
+histGrid <- function(i, j) {
+  d <- group_by_at(i, vars(station, species, year))
+  dl <- group_split(d)
+  pd <- lapply(dl, plotHist)
+  plots <- do.call("grid.arrange", c(pd, ncol = 2, top = print(j)))
 }
