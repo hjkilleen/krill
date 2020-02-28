c85c01a49a2b785b79b04b12d5dc2de757bfe7a7
Helen Killeen <hjkilleen@ucdavis.edu>
2020-02-28 14:56:44 -0800

    Change regions to sites and add coarser regional classification.

--- scripts/functions/regions.R
+++ scripts/functions/regions.R
@@ -1,4 +1,4 @@
-#List of regions and corresponding stations
+#List of sites and corresponding stations
 FortRoss <- c(452, 454)
 PointReyes <- c(166, 170)
 GulfFarallons <- c(139, 156)
@@ -14,8 +14,9 @@ MidChannelIslands <- c(411, 414)
 SouthernCA <- c(482, 402)
 
 #As a dataframe 
-regions <- data.frame(
-  region = c("FortRoss", "PointReyes", "GulfFarallons", "SanMateo", "Davenport", "MontereyBay", "PointSur", "PiedrasBlancas", "MorroBay", "PointConception", "SantaBarbara", "MidChannelIslands", "SouthernCA"),
+sites <- data.frame(
+  site = c("FortRoss", "PointReyes", "GulfFarallons", "SanMateo", "Davenport", "MontereyBay", "PointSur", "PiedrasBlancas", "MorroBay", "PointConception", "SantaBarbara", "MidChannelIslands", "SouthernCA"),
   onshore = c(452, 166, 139, 132, 124, 114, 102, 442, 492, 433, 422, 411, 482),
-  offshore = c(454, 170, 156, 134, 127, 110, 105, 445, 495, 435, 425, 414, 402)
+  offshore = c(454, 170, 156, 134, 127, 110, 105, 445, 495, 435, 425, 414, 402),
+  region = c("north", "north", "northCentral", "northCentral", "northCentral", "northCentral", "northCentral", "central", "central", "central", "south", "south", "south")
 )
