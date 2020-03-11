f4490964fbe5e29811c668416da9a35a874183c3
Helen Killeen <hjkilleen@ucdavis.edu>
2020-03-11 12:35:48 -0700

    Make site dataframes for each year

--- scripts/functions/regions.R
+++ scripts/functions/regions.R
@@ -1,22 +1,43 @@
 #List of sites and corresponding stations
-FortRoss <- c(452, 454)
-PointReyes <- c(166, 170)
-GulfFarallons <- c(139, 156)
-SanMateo <- c(132, 134)
+FortRoss <- c(183, 452, 453, 454)
+PointReyes <- c(166, 167, 170)
+GulfFarallons <- c(138, 139, 152, 156)
+SanMateo <- c(131, 132, 134)
 Davenport <- c(124, 127)
-MontereyBay <- c(114, 110, 117)
+MontereyBay <- c(114, 112, 110, 117)
 PointSur <- c(102, 105)
 PiedrasBlancas <- c(442, 445)
-MorroBay <- c(492, 495)
+MorroBay <- c(492, 493, 495)
 PointConception <- c(433, 435)
 SantaBarbara <- c(422, 425)
-MidChannelIslands <- c(411, 414)
-SouthernCA <- c(482, 402)
+MidChannelIslands <- c(411, 412, 413, 414)
+SouthernCA <- c(481, 482, 402)
 
 #As a dataframe 
-sites <- data.frame(
-  site = c("FortRoss", "PointReyes", "GulfFarallons", "SanMateo", "Davenport", "MontereyBay", "PointSur", "PiedrasBlancas", "MorroBay", "PointConception", "SantaBarbara", "MidChannelIslands", "SouthernCA"),
-  onshore = c(452, 166, 139, 132, 124, 114, 102, 442, 492, 433, 422, 411, 482),
-  offshore = c(454, 170, 156, 134, 127, 110, 105, 445, 495, 435, 425, 414, 402),
-  region = c("north", "north", "northCentral", "northCentral", "northCentral", "northCentral", "northCentral", "central", "central", "central", "south", "south", "south")
+sites2015 <- data.frame(
+  site = c("FortRoss", "PiedrasBlancas", "MorroBay", "SantaBarbara", "MidChannelIslands", "SouthernCA"),
+  onshore = c(453, 442, NA, 422, 411, 481),
+  offshore = c(454, NA, 495, 425, 413, 402),
+  region = c("north", "central", "central", "south", "south", "south")
+)
+
+sites2016 <- data.frame(
+  site = c("FortRoss", "PointReyes", "GulfFarallons", "SanMateo", "Davenport", "MontereyBay", "PiedrasBlancas", "MorroBay", "SantaBarbara", "MidChannelIslands", "SouthernCA"),
+  onshore = c(453, 167, 138, 132, 124, 112, 442, 493, 422, 411, 482),
+  offshore = c(454, 170, 152, 134, 127, 110, 445, 495, 425, 414, 402),
+  region = c("north", "north", "northCentral", "northCentral", "northCentral", "northCentral", "central", "central", "south", "south", "south")
+)
+
+sites2017 <- data.frame(
+  site = c("FortRoss", "PointReyes", "GulfFarallons", "SanMateo", "Davenport", "MontereyBay", "PiedrasBlancas", "MorroBay", "SantaBarbara", "MidChannelIslands", "SouthernCA"),
+  onshore = c(453, 167, 138, 132, 124, 112, 442, NA, 422, 412, 482),
+  offshore = c(NA, 171, 156, 134, 127, 117, 445, 495, 425, NA, 402),
+  region = c("north", "north", "northCentral", "northCentral", "northCentral", "northCentral", "central", "central", "south", "south", "south")
+)
+
+sites2018 <- data.frame(
+  site = c("FortRoss", "PointReyes", "GulfFarallons", "SanMateo", "Davenport", "MontereyBay", "PiedrasBlancas", "MorroBay", "SantaBarbara", "MidChannelIslands", "SouthernCA"),
+  onshore = c(183, 167, 139, 132, 124, 114, 442, NA, 422, 411, 481),
+  offshore = c(454, 170, 156, 134, 127, 110, 445, 495, 425, 413, 402),
+  region = c("north", "north", "northCentral", "northCentral", "northCentral", "northCentral", "central", "central", "south", "south", "south")
 )
