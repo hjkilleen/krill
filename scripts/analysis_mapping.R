57ac16cd581cc6460546688401d5f9634b8148fe
Helen Killeen <hjkilleen@ucdavis.edu>
2020-03-11 15:56:57 -0700

    Get rid of redundant code for station maps.

--- scripts/analysis_mapping.R
+++ scripts/analysis_mapping.R
@@ -17,6 +17,7 @@ ggplot(data = coast) +
   geom_sf()
 
 #Station maps for each year
+#=======
 #read in RF dataset for Euphausia
 rf <- read_xlsx("../../PhD/Research/Krill/RF_Survey_Data_EUPHAUSIDAE.xlsx")
 rf$time <- as_datetime(rf$time)
@@ -60,32 +61,10 @@ mapStations(summarize(
   lon = mean(longitude)
 ))
 dev.off()
+#=======
 
-#Plot map of the CCE with stations we looked at for length frequency in 2015
-ggplot(data = world) +
-  geom_sf(color = "black", fill = "lightgreen") +
-  geom_sf(data = states, fill = NA) +
-  annotation_scale(location = "bl", width_hint = 0.5) +
-  geom_point(data = t, aes(x = lon, y = lat), size = 2, 
-             shape = 23, fill = "darkred") +
-  annotation_north_arrow(location = "bl", which_north = "true", 
-                         pad_x = unit(0.2, "in"), pad_y = unit(0.3, "in"),
-                         style = north_arrow_fancy_orienteering) +
-  coord_sf(xlim = c(-125.5, -116.75), ylim = c(32.0, 42.30), expand = FALSE)
-
-#What about the missing areas and the stations 358, 421, 426, 427, and 477 that don't appear to have station coordinates, or to have been sampled during 2015 according to the RF metadata?
-#ALSO, what about the other stations on Jeff's priority list that don't appear to have been measured?
-
-#let's look in the krill counts, to see whether or not we ever looked through these stations (2015)
-cts <- read_xlsx("data/Krillcounts.xlsx",2)
-cts <- cts[,1:10]
-names <- c("year", "id", "sample", "haul", "station", "species", "split", "male", "female", "total")
-names(cts) <- names
-
-cts15 <- filter(cts, year == 2015)
-View(unique(cts15$station))
-
-#SUPER NORTH
+#SUPER NORTH Map
+#=======
 #Map out northern CA stations to see whether or not it's worth adding a new line north of Fort Ross
 #get relevant stations
 rf <- read_xlsx("../../PhD/Research/Krill/RF_Survey_Data_EUPHAUSIDAE.xlsx")
@@ -107,3 +86,8 @@ ggplot(data = world) +
                          pad_x = unit(0.2, "in"), pad_y = unit(0.3, "in"),
                          style = north_arrow_fancy_orienteering) +
   coord_sf(xlim = c(-125.5, -124.0), ylim = c(40.0, 42.30), expand = FALSE)
+#========
+
+
+
+
