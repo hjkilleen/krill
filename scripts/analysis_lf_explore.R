dcc7d4fdbc50c641c67e776a152c16f2e9e3c1f8
Helen Killeen <hjkilleen@ucdavis.edu>
2020-06-10 07:43:30 -0700

    Filter species in lengths.

--- scripts/analysis_lf_explore.R
+++ scripts/analysis_lf_explore.R
@@ -29,6 +29,7 @@ add_tally(group_by_at(lengths, vars(year, station, species, sex)))
 #filter to only the stations we are including in the analysis
 lengths <- filter(lengths, station %in% allSites)
 lengths <- left_join(lengths, regions, by = "station")
+lengths <- filter(lengths, species == "EP" | species == "TS" | species == "ND")
 save(lengths, file = "data/lengths.rda")
 #load 2011&2012 data and merge with 2015-2018 dataset
 lengthsBaldo <- read.csv("data/lengthsBaldo.csv")
