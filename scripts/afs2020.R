b9f258f9d672f85bc6f2cd7610079a0f2d4f56a5
Helen Killeen <hjkilleen@ucdavis.edu>
2020-08-20 06:00:23 -0700

    Bigger.

--- scripts/afs2020.R
+++ scripts/afs2020.R
@@ -13,7 +13,7 @@ ggplot(filter(ep), aes(x = year, y = length, fill = year)) +
   geom_text(data = summarize(group_by(ep, year), mean = round(mean(length), 1), max = max(length)), aes(x = year, y = max + 4, label = paste("mean=", mean, sep = " ")), color = "black", size = 3) +
   labs(x = "Year", y = "Length (mm)", title = "E. pacifica lengths by year") + 
   theme(text = element_text(size = 20)) +
-  ggsave("figures/bodySize/EP/byYear.jpg", width = 7, height = 9)
+  ggsave("figures/bodySize/EP/byYear.jpg", width = 9, height = 9)
 
 #Figure 1b - T. spinifera lengths by year
 #ts <- add_tally(group_by_at(ts, vars(year)))
@@ -25,7 +25,7 @@ ggplot(filter(ts), aes(x = year, y = length, fill = year)) +
   geom_text(data = summarize(group_by(ts, year), mean = round(mean(length), 1), max = max(length)), aes(x = year, y = max + 4, label = paste("mean=", mean, sep = " ")), color = "black", size = 3) +
   labs(x = "Year", y = "Length (mm)", title = "T. spinifera lengths by year") + 
   theme(text = element_text(size = 20)) +
-  ggsave("figures/bodySize/TS/byYear.jpg", width = 7, height = 9)
+  ggsave("figures/bodySize/TS/byYear.jpg", width = 9, height = 9)
 
 #Figure 1c - N. difficilis lengths by year
 #nd <- add_tally(group_by_at(nd, vars(year)))
@@ -37,6 +37,6 @@ ggplot(filter(nd), aes(x = year, y = length, fill = year)) +
   geom_text(data = summarize(group_by(nd, year), mean = round(mean(length), 1), max = max(length)), aes(x = year, y = max + 4, label = paste("mean=", mean, sep = " ")), color = "black", size = 3) +
   labs(x = "Year", y = "Length (mm)", title = "N. difficilis lengths by year") + 
   theme(text = element_text(size = 20)) +
-  ggsave("figures/bodySize/ND/byYear.jpg", width = 7, height = 9)
+  ggsave("figures/bodySize/ND/byYear.jpg", width = 9, height = 9)
 
 
