dbc0317e479357240756cfdfe8eb26438f1434b3
Helen Killeen <hjkilleen@ucdavis.edu>
2020-08-20 05:50:29 -0700

    Add labels for n and mean values.

--- scripts/afs2020.R
+++ scripts/afs2020.R
@@ -2,30 +2,41 @@
 #Figures for presentation given at the American Fisheries Society Annual Meeting in September 2020
 
 # Thu Aug 13 19:30:48 2020 ------------------------------
-
+  
 #Figure 1a - E. pacfica lengths by year
+#ep <- add_tally(group_by_at(ep, vars(year)))
 ggplot(filter(ep), aes(x = year, y = length, fill = year)) +
   geom_violin() +
   geom_boxplot(width = 0.1) +
   scale_fill_manual(values = c("#ffffff80", "#00000080", "#ffffff50", "#ff000080", "#00ff0080", "#0000ff80", "#ffff0080"), labels = c("2011", "2012", "2013", "2015", "2016", "2017", "2018")) +
+  geom_text(data = summarize(group_by(ep, year), n=mean(n), max = max(length)), aes(x = year, y = max + 2, label = paste("n=", n, sep = " ")), color = "black", size = 3) +
+  geom_text(data = summarize(group_by(ep, year), mean = round(mean(length), 1), max = max(length)), aes(x = year, y = max + 4, label = paste("mean=", mean, sep = " ")), color = "black", size = 3) +
   labs(x = "Year", y = "Length (mm)", title = "E. pacifica lengths by year") + 
   theme(text = element_text(size = 20)) +
   ggsave("figures/bodySize/EP/byYear.jpg", width = 7, height = 9)
 
 #Figure 1b - T. spinifera lengths by year
+#ts <- add_tally(group_by_at(ts, vars(year)))
 ggplot(filter(ts), aes(x = year, y = length, fill = year)) +
   geom_violin() +
   geom_boxplot(width = 0.1) +
   scale_fill_manual(values = c("#ffffff80", "#00000080", "#ffffff50", "#ff000080", "#00ff0080", "#0000ff80", "#ffff0080"), labels = c("2011", "2012", "2013", "2015", "2016", "2017", "2018")) +
+  geom_text(data = summarize(group_by(ts, year), n=mean(n), max = max(length)), aes(x = year, y = max + 2, label = paste("n=", n, sep = " ")), color = "black", size = 3) +
+  geom_text(data = summarize(group_by(ts, year), mean = round(mean(length), 1), max = max(length)), aes(x = year, y = max + 4, label = paste("mean=", mean, sep = " ")), color = "black", size = 3) +
   labs(x = "Year", y = "Length (mm)", title = "T. spinifera lengths by year") + 
   theme(text = element_text(size = 20)) +
   ggsave("figures/bodySize/TS/byYear.jpg", width = 7, height = 9)
 
 #Figure 1c - N. difficilis lengths by year
+#nd <- add_tally(group_by_at(nd, vars(year)))
 ggplot(filter(nd), aes(x = year, y = length, fill = year)) +
   geom_violin() +
   geom_boxplot(width = 0.1) +
   scale_fill_manual(values = c("#ffffff80", "#00000080", "#ffffff50", "#ff000080", "#00ff0080", "#0000ff80", "#ffff0080"), labels = c("2011", "2012", "2013", "2015", "2016", "2017", "2018")) +
+  geom_text(data = summarize(group_by(nd, year), n=mean(n), max = max(length)), aes(x = year, y = max + 2, label = paste("n=", n, sep = " ")), color = "black", size = 3) +
+  geom_text(data = summarize(group_by(nd, year), mean = round(mean(length), 1), max = max(length)), aes(x = year, y = max + 4, label = paste("mean=", mean, sep = " ")), color = "black", size = 3) +
   labs(x = "Year", y = "Length (mm)", title = "N. difficilis lengths by year") + 
   theme(text = element_text(size = 20)) +
   ggsave("figures/bodySize/ND/byYear.jpg", width = 7, height = 9)
+
+
