74a166a61b5e56f674d62165c7c437e596635710
Helen Killeen <hjkilleen@ucdavis.edu>
2020-08-19 17:02:22 -0700

    Add figure 1.

--- scripts/afs2020.R
+++ scripts/afs2020.R
@@ -3,3 +3,29 @@
 
 # Thu Aug 13 19:30:48 2020 ------------------------------
 
+#Figure 1a - E. pacfica lengths by year
+ggplot(filter(ep), aes(x = year, y = length, fill = year)) +
+  geom_violin() +
+  geom_boxplot(width = 0.1) +
+  scale_fill_manual(values = c("#ffffff80", "#00000080", "#ffffff50", "#ff000080", "#00ff0080", "#0000ff80", "#ffff0080"), labels = c("2011", "2012", "2013", "2015", "2016", "2017", "2018")) +
+  labs(x = "Year", y = "Length (mm)", title = "E. pacifica lengths by year") + 
+  theme(text = element_text(size = 20)) +
+  ggsave("figures/bodySize/EP/byYear.jpg", width = 7, height = 9)
+
+#Figure 1b - T. spinifera lengths by year
+ggplot(filter(ts), aes(x = year, y = length, fill = year)) +
+  geom_violin() +
+  geom_boxplot(width = 0.1) +
+  scale_fill_manual(values = c("#ffffff80", "#00000080", "#ffffff50", "#ff000080", "#00ff0080", "#0000ff80", "#ffff0080"), labels = c("2011", "2012", "2013", "2015", "2016", "2017", "2018")) +
+  labs(x = "Year", y = "Length (mm)", title = "T. spinifera lengths by year") + 
+  theme(text = element_text(size = 20)) +
+  ggsave("figures/bodySize/TS/byYear.jpg", width = 7, height = 9)
+
+#Figure 1c - N. difficilis lengths by year
+ggplot(filter(nd), aes(x = year, y = length, fill = year)) +
+  geom_violin() +
+  geom_boxplot(width = 0.1) +
+  scale_fill_manual(values = c("#ffffff80", "#00000080", "#ffffff50", "#ff000080", "#00ff0080", "#0000ff80", "#ffff0080"), labels = c("2011", "2012", "2013", "2015", "2016", "2017", "2018")) +
+  labs(x = "Year", y = "Length (mm)", title = "N. difficilis lengths by year") + 
+  theme(text = element_text(size = 20)) +
+  ggsave("figures/bodySize/ND/byYear.jpg", width = 7, height = 9)
