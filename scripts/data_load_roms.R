0fdcfc0aa08ca696c57c9e8eb6b27c1e6c974b4b
Helen Killeen <hjkilleen@ucdavis.edu>
2020-08-20 09:09:59 -0700

    comment lines for avoid rerunning for loop.

--- scripts/data_load_roms.R
+++ scripts/data_load_roms.R
@@ -9,51 +9,54 @@ load("data/allLengths.rda")
 stations <- read.csv("data/stationMetadata.csv")
 station <- summarize(group_by(stations, station), lat = mean(station_latitude), lon = mean(station_longitude), depth = mean(bottom_depth))
 urls <- read.csv("data/urls.csv")
-for(i in seq(1:nrow(urls))) {
-  #download file and read as csv
-  temporary <- tempfile()
-  download.file(as.character(urls$url[i]),temporary)
-  tempData <- read.csv(unz(temporary, "temp.csv"))
-  unlink(temporary)
-  #rename data
-  tempNames <- names(tempData[,6:ncol(tempData)])
-  tempNames <- str_extract(tempNames[], "(\\d)+")
-  metaNames <- c("x", "y", "lat", "lon", "datetime")
-  allNames <- c(metaNames, tempNames)
-  names(tempData) <- allNames
-  #dates
-  tempData$date <-as.Date(str_extract(as.character(tempData$datetime), "[^T]+"))
-  tempData <- mutate(tempData, year = lubridate::year(date), 
-                     month = lubridate::month(date), 
-                     day = lubridate::day(date))
-  tempData$monthDay <- paste(tempData$month, tempData$day, sep = ".")
-  dates <- c("5.1", "5.2", "5.3", "5.4", "5.5", "5.6", "5.7", "5.8", "5.9", "5.10", "5.11", "5.12", "5.13", "5.14", "5.15", "5.16", "5.17", "5.18", "5.19", "5.20", "5.21", "5.22", "5.23", "5.24", "5.25", "5.26", "5.27", "5.28", "5.29", "5.30", "5.31", "6.1", "6.2", "6.3", "6.4", "6.5", "6.6", "6.7", "6.8", "6.9", "6.10", "6.11", "6.12", "6.13", "6.14", "6.15")
-  #filtering
-  if("100" %in% names(tempData)){
-    temps <- select(tempData, lat, lon, year, month, day, monthDay, "2", "100")
-    names(temps) <- c("lat", "lon", "year", "month", "day", "monthDay", "temp_2", "temp_100")
-  } else {
-    temps <- select(tempData, lat, lon, year, month, day, monthDay, "2")
-    names(temps) <- c("lat", "lon", "year", "month", "day", "monthDay", "temp_2")
-  }
-  temps$lat_lon <- paste(temps$lat, temps$lon, sep = "_")
-  temps <- filter(temps, year >= 2011, year <=2018)
-  temps <- filter(temps, year != 2014)
-  temps <- filter(temps, monthDay %in% dates)
-  #convert temp to Celsius
-  temps$temp_2 <- (temps$temp_2-32)/1.8
-  if("temp_100" %in% names(temps)){
-    temps$temp_100 <- (temps$temp_100-32)/1.8
-  }
-  #grouping
-  if("temp_100" %in% names(temps)){
-    temp <- summarize(group_by(temps, year), lat = mean(lat), lon = mean(lon), temp_2 = mean(temp_2), temp_100 = mean(temp_100))
-  } else {
-    temp <- summarize(group_by(temps, year), lat = mean(lat), lon = mean(lon), temp_2 = mean(temp_2))
-  }
-  #write .csv
-  write.csv(temp, file = paste("data/roms_temperatures/", urls$station[i], "_temp.csv", sep = ""))
-}
+
+#UNCOMMENT BELOW TO ADD NEW ROMS DATA FOR NEW STATIONS. 
+
+# for(i in seq(1:nrow(urls))) {
+#   #download file and read as csv
+#   temporary <- tempfile()
+#   download.file(as.character(urls$url[i]),temporary)
+#   tempData <- read.csv(unz(temporary, "temp.csv"))
+#   unlink(temporary)
+#   #rename data
+#   tempNames <- names(tempData[,6:ncol(tempData)])
+#   tempNames <- str_extract(tempNames[], "(\\d)+")
+#   metaNames <- c("x", "y", "lat", "lon", "datetime")
+#   allNames <- c(metaNames, tempNames)
+#   names(tempData) <- allNames
+#   #dates
+#   tempData$date <-as.Date(str_extract(as.character(tempData$datetime), "[^T]+"))
+#   tempData <- mutate(tempData, year = lubridate::year(date), 
+#                      month = lubridate::month(date), 
+#                      day = lubridate::day(date))
+#   tempData$monthDay <- paste(tempData$month, tempData$day, sep = ".")
+#   dates <- c("5.1", "5.2", "5.3", "5.4", "5.5", "5.6", "5.7", "5.8", "5.9", "5.10", "5.11", "5.12", "5.13", "5.14", "5.15", "5.16", "5.17", "5.18", "5.19", "5.20", "5.21", "5.22", "5.23", "5.24", "5.25", "5.26", "5.27", "5.28", "5.29", "5.30", "5.31", "6.1", "6.2", "6.3", "6.4", "6.5", "6.6", "6.7", "6.8", "6.9", "6.10", "6.11", "6.12", "6.13", "6.14", "6.15")
+#   #filtering
+#   if("100" %in% names(tempData)){
+#     temps <- select(tempData, lat, lon, year, month, day, monthDay, "2", "100")
+#     names(temps) <- c("lat", "lon", "year", "month", "day", "monthDay", "temp_2", "temp_100")
+#   } else {
+#     temps <- select(tempData, lat, lon, year, month, day, monthDay, "2")
+#     names(temps) <- c("lat", "lon", "year", "month", "day", "monthDay", "temp_2")
+#   }
+#   temps$lat_lon <- paste(temps$lat, temps$lon, sep = "_")
+#   temps <- filter(temps, year >= 2011, year <=2018)
+#   temps <- filter(temps, year != 2014)
+#   temps <- filter(temps, monthDay %in% dates)
+#   #convert temp to Celsius
+#   temps$temp_2 <- (temps$temp_2-32)/1.8
+#   if("temp_100" %in% names(temps)){
+#     temps$temp_100 <- (temps$temp_100-32)/1.8
+#   }
+#   #grouping
+#   if("temp_100" %in% names(temps)){
+#     temp <- summarize(group_by(temps, year), lat = mean(lat), lon = mean(lon), temp_2 = mean(temp_2), temp_100 = mean(temp_100))
+#   } else {
+#     temp <- summarize(group_by(temps, year), lat = mean(lat), lon = mean(lon), temp_2 = mean(temp_2))
+#   }
+#   #write .csv
+#   write.csv(temp, file = paste("data/roms_temperatures/", urls$station[i], "_temp.csv", sep = ""))
+# }
 
 #Load csv files
 mydir = "data/roms_temperatures/"
@@ -67,4 +70,4 @@ waterTemp <- select(waterTemp, year, station, temp_2, temp_100)
 allLengths$stationYear <- paste(allLengths$station, allLengths$year, sep = "")
 waterTemp$stationYear <- paste(waterTemp$station, waterTemp$year, sep = "")
 allLengthsEnv <- left_join(allLengths, waterTemp, by = "stationYear")
-save(allLengthsEnv, "data/allLengthsEnv.rda")
+save(allLengthsEnv, file = "data/allLengthsEnv.rda")
