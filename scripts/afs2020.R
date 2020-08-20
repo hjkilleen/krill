9332e79ad0964e041131df0a8c910b5f82834ff3
Helen Killeen <hjkilleen@ucdavis.edu>
2020-08-20 10:39:44 -0700

    Add figures to update those presented at NRT symposium.

--- scripts/afs2020.R
+++ scripts/afs2020.R
@@ -13,7 +13,7 @@ ggplot(filter(ep), aes(x = year, y = length, fill = year)) +
   geom_text(data = summarize(group_by(ep, year), mean = round(mean(length), 1), max = max(length)), aes(x = year, y = max + 4, label = paste("mean=", mean, sep = " ")), color = "black", size = 3) +
   labs(x = "Year", y = "Length (mm)", title = "E. pacifica lengths by year") + 
   theme(text = element_text(size = 20)) +
-  ggsave("figures/bodySize/EP/byYear.jpg", width = 9, height = 9)
+  ggsave("figures/afs2020/EPbyYear.jpg", width = 9, height = 9)
 
 #Figure 1b - T. spinifera lengths by year
 #ts <- add_tally(group_by_at(ts, vars(year)))
@@ -25,7 +25,7 @@ ggplot(filter(ts), aes(x = year, y = length, fill = year)) +
   geom_text(data = summarize(group_by(ts, year), mean = round(mean(length), 1), max = max(length)), aes(x = year, y = max + 4, label = paste("mean=", mean, sep = " ")), color = "black", size = 3) +
   labs(x = "Year", y = "Length (mm)", title = "T. spinifera lengths by year") + 
   theme(text = element_text(size = 20)) +
-  ggsave("figures/bodySize/TS/byYear.jpg", width = 9, height = 9)
+  ggsave("figures/afs2020/TSbyYear.jpg", width = 9, height = 9)
 
 #Figure 1c - N. difficilis lengths by year
 #nd <- add_tally(group_by_at(nd, vars(year)))
@@ -37,6 +37,137 @@ ggplot(filter(nd), aes(x = year, y = length, fill = year)) +
   geom_text(data = summarize(group_by(nd, year), mean = round(mean(length), 1), max = max(length)), aes(x = year, y = max + 4, label = paste("mean=", mean, sep = " ")), color = "black", size = 3) +
   labs(x = "Year", y = "Length (mm)", title = "N. difficilis lengths by year") + 
   theme(text = element_text(size = 20)) +
-  ggsave("figures/bodySize/ND/byYear.jpg", width = 9, height = 9)
+  ggsave("figures/afs2020/NDbyYear.jpg", width = 9, height = 9)
 
+#Figure 2a - Differences among species in response to surface temperature
+# simM9 <- fsim.glmm(M9)
+# simsumM9 <- simsum(simM9)
+sum <- summarize(group_by_at(simsumM9, vars(species, temp_2)), sim.mean = mean(sim.mean), lower.95 = mean(lower.95), upper.95 = mean(upper.95))
+ggplot() + 
+  geom_point(data = allLengthsEnv, aes(x = temp_2, y = length, color = as.factor(species)), alpha = 0.1) + #geom_ribbon(data = sum, aes(x = as.numeric(year), ymin = lower.95, ymax = upper.95, fill = species), alpha = 0.2) + 
+  #facet_wrap(as.factor(allLengthsEnv$species)) + 
+  geom_smooth(data = sum, aes(x = temp_2, y = sim.mean, color = species)) + 
+  labs(y = "Length (mm)", x = "Temp (C)", title = "Simulated krill lengths by temperature at surface") +
+  theme(text = element_text(size = 20)) + 
+  ggsave("figures/afs2020/fig2a.jpg", width = 9, height = 7)
 
+#Figure 2b - Differences among species in response to temperature at 100 m
+sum <- summarize(group_by_at(simsumM9, vars(species, temp_100)), sim.mean = mean(sim.mean), lower.95 = mean(lower.95), upper.95 = mean(upper.95))
+ggplot() + 
+  geom_point(data = allLengthsEnv, aes(x = temp_100, y = length, color = as.factor(species)), alpha = 0.1) + #geom_ribbon(data = sum, aes(x = as.numeric(year), ymin = lower.95, ymax = upper.95, fill = species), alpha = 0.2) + 
+  #facet_wrap(as.factor(allLengthsEnv$species)) + 
+  geom_smooth(data = sum, aes(x = temp_100, y = sim.mean, color = species)) + 
+  labs(y = "Length (mm)", x = "Temp (C)", title = "Simulated krill lengths by temperature at 100 m") +
+  theme(text = element_text(size = 20)) + 
+  ggsave("figures/afs2020/fig2b.jpg", width = 9, height = 7)
+
+#Figure 3a - Model with different effects north and south of PC
+x <- filter(allLengthsEnvDep, latitude >34.4)
+y <- filter(allLengthsEnvDep, latitude <=34.4)
+x <- mutate(x, NS = "N")
+y <- mutate(y, NS = "S")
+NS <-rbind(x, y)
+MNS <- lmer(length ~ species*sex + temp_2 + species:temp_2 + NS:temp_2 + sex:temp_2 + temp_100 + species:temp_100 + NS:temp_100 + sex:temp_100 + dist + species:dist + (1|station.x), data = NS, REML = FALSE)
+
+simMNS <- fsim.glmm(MNS)
+simsumMNS <- simsum(simMNS)
+
+ggplot() +
+  geom_point(data = NS, aes(temp_2, length, color = species), alpha = 0.2) + 
+  geom_smooth(data = simsumMNS, method = "lm", aes(x = temp_2, y = sim.mean, color = species)) +
+  facet_wrap(~NS, nrow=1, ncol=2, scales = "free_y") +
+  labs(y = "Length (mm)", x = "Temp (C)", title = "Krill lengths by temperature at surface N vs. S") +
+  theme(text = element_text(size = 20)) + 
+  ggsave("figures/afs2020/fig3a.jpg", width = 9, height = 7)
+
+#Figure 3b - Model with different effects north and south of PC
+ggplot() +
+  geom_point(data = NS, aes(temp_100, length, color = species), alpha = 0.2) + 
+  geom_smooth(data = simsumMNS, method = "lm", aes(x = temp_100, y = sim.mean, color = species)) +
+  facet_wrap(~NS, nrow=1, ncol=2, scales = "free_y") +
+  labs(y = "Length (mm)", x = "Temp (C)", title = "Krill lengths by temperature at 100 m N vs. S") +
+  theme(text = element_text(size = 20)) + 
+  ggsave("figures/afs2020/fig3b.jpg", width = 9, height = 7)
+
+#Figure 4a - Differences among regions 
+sum <- summarize(group_by_at(simsumM9, vars(species, temp_2, region)), sim.mean = mean(sim.mean), lower.95 = mean(lower.95), upper.95 = mean(upper.95))
+ggplot() + 
+  geom_point(data = filter(allLengthsEnv, region != "NA"), aes(x = temp_2, y = length, color = as.factor(species)), alpha = 0.1) + #geom_ribbon(data = sum, aes(x = as.numeric(year), ymin = lower.95, ymax = upper.95, fill = species), alpha = 0.2) + 
+  geom_smooth(data = sum, aes(x = temp_2, y = sim.mean, color = species)) + 
+  facet_wrap(~region) +
+  labs(y = "Length (mm)", x = "Temp (C)", title = "Simulated krill lengths by temperature at surface for each region") +
+  theme(text = element_text(size = 20)) + 
+  ggsave("figures/afs2020/fig4a.jpg", width = 9, height = 7)
+
+#Figure 4b - Differences among regions 
+sum <- summarize(group_by_at(simsumM9, vars(species, temp_100, region)), sim.mean = mean(sim.mean), lower.95 = mean(lower.95), upper.95 = mean(upper.95))
+ggplot() + 
+  geom_point(data = filter(allLengthsEnv, region != "NA"), aes(x = temp_100, y = length, color = as.factor(species)), alpha = 0.1) + #geom_ribbon(data = sum, aes(x = as.numeric(year), ymin = lower.95, ymax = upper.95, fill = species), alpha = 0.2) + 
+  geom_smooth(data = sum, aes(x = temp_100, y = sim.mean, color = species)) + 
+  facet_wrap(~region) +
+  labs(y = "Length (mm)", x = "Temp (C)", title = "Simulated krill lengths by temperature at 100 m for each region") +
+  theme(text = element_text(size = 20)) + 
+  ggsave("figures/afs2020/fig4b.jpg", width = 9, height = 7)
+
+#4c - Differences among regions using year instead of temperature (M3)
+# simM3 <- fsim.glmm(M3) 
+# simsumM3 <- simsum(simM3)
+
+sum <- summarize(group_by_at(simsumM3, vars(species, year, region)), sim.mean = mean(sim.mean), lower.95 = mean(lower.95), upper.95 = mean(upper.95))
+ggplot() + 
+  geom_point(data = filter(allLengths, region != "NA"), aes(x = year, y = length, color = species), alpha = 0.1) + 
+  facet_wrap(vars(region)) +   
+  #geom_ribbon(data = sum, aes(x = as.numeric(year), ymin = lower.95, ymax = upper.95, fill = species), alpha = 0.2) + 
+  geom_line(data = sum, aes(x = as.numeric(year), y = sim.mean, color = species)) + 
+  geom_rect(data = sum, aes(xmin = 4, xmax = 6, ymin = 0, ymax = Inf), alpha = 0.01, fill = "red") +
+  labs(y = "Length (mm)", x = "Year", title = "Krill lengths during a marine heatwave") +
+  theme(text = element_text(size = 20)) + 
+  ggsave("figures/afs2020/fig4c.jpg", width = 9, height = 7)
+
+#Figure 5a - Differences among regions using year instead of temperature (M3), sex
+sum <- summarize(group_by_at(simsumM3, vars(species, year, region, sex)), sim.mean = mean(sim.mean), lower.95 = mean(lower.95), upper.95 = mean(upper.95))
+ggplot() + 
+  geom_point(data = filter(allLengths, region != "NA"), aes(x = year, y = length, color = species), alpha = 0.1) + 
+  facet_wrap(vars(region)) +   
+  #geom_ribbon(data = sum, aes(x = as.numeric(year), ymin = lower.95, ymax = upper.95, fill = species), alpha = 0.2) + 
+  geom_line(data = sum, aes(x = as.numeric(year), y = sim.mean, color = species, linetype = sex)) + 
+  geom_rect(data = sum, aes(xmin = 4, xmax = 6, ymin = 0, ymax = Inf), alpha = 0.01, fill = "red") +
+  labs(y = "Length (mm)", x = "Year", title = "Krill lengths during a marine heatwave - sex") +
+  theme(text = element_text(size = 20)) + 
+  ggsave("figures/afs2020/fig5a.jpg", width = 9, height = 7)
+
+#Figure 5b - Same without 2013
+sum <- filter(summarize(group_by_at(simsumM3, vars(species, year, region, sex)), sim.mean = mean(sim.mean), lower.95 = mean(lower.95), upper.95 = mean(upper.95)), year != 2013)
+ggplot() + 
+  geom_point(data = filter(allLengths, region != "NA", year !=2013), aes(x = year, y = length, color = species), alpha = 0.1) + 
+  facet_wrap(vars(region)) +   
+  #geom_ribbon(data = sum, aes(x = as.numeric(year), ymin = lower.95, ymax = upper.95, fill = species), alpha = 0.2) + 
+  geom_line(data = sum, aes(x = as.numeric(year), y = sim.mean, color = species, linetype = sex)) + 
+  geom_rect(data = sum, aes(xmin = 3, xmax = 5, ymin = 0, ymax = Inf), alpha = 0.01, fill = "red") +
+  labs(y = "Length (mm)", x = "Year", title = "Krill lengths during a marine heatwave - sex") +
+  theme(text = element_text(size = 20)) + 
+  ggsave("figures/afs2020/fig5b.jpg", width = 9, height = 7)
+
+#Figure 6 - Differences among regions using year instead of temperature (M3), shore
+sum <- summarize(group_by_at(simsumM3, vars(species, year, region, shore)), sim.mean = mean(sim.mean), lower.95 = mean(lower.95), upper.95 = mean(upper.95))
+ggplot() + 
+  geom_point(data = filter(allLengths, region != "NA"), aes(x = year, y = length, color = species), alpha = 0.1) + 
+  facet_wrap(vars(region)) +   
+  #geom_ribbon(data = sum, aes(x = as.numeric(year), ymin = lower.95, ymax = upper.95, fill = species), alpha = 0.2) + 
+  geom_line(data = sum, aes(x = as.numeric(year), y = sim.mean, color = species, linetype = shore)) + 
+  geom_rect(data = sum, aes(xmin = 4, xmax = 6, ymin = 0, ymax = Inf), alpha = 0.01, fill = "red") +
+  labs(y = "Length (mm)", x = "Year", title = "Krill lengths during a marine heatwave - shore") +
+  theme(text = element_text(size = 20)) + 
+  ggsave("figures/afs2020/fig6a.jpg", width = 9, height = 7)
+
+#Figure 6b - Same without 2013
+sum <- filter(summarize(group_by_at(simsumM3, vars(species, year, region, shore)), sim.mean = mean(sim.mean), lower.95 = mean(lower.95), upper.95 = mean(upper.95)), year != 2013)
+ggplot() + 
+  geom_point(data = filter(allLengths, region != "NA", year !=2013), aes(x = year, y = length, color = species), alpha = 0.1) + 
+  facet_wrap(vars(region)) +   
+  #geom_ribbon(data = sum, aes(x = as.numeric(year), ymin = lower.95, ymax = upper.95, fill = species), alpha = 0.2) + 
+  geom_line(data = sum, aes(x = as.numeric(year), y = sim.mean, color = species, linetype = shore)) + 
+  geom_rect(data = sum, aes(xmin = 3, xmax = 5, ymin = 0, ymax = Inf), alpha = 0.01, fill = "red") +
+  labs(y = "Length (mm)", x = "Year", title = "Krill lengths during a marine heatwave - shore") +
+  theme(text = element_text(size = 20)) + 
+  ggsave("figures/afs2020/fig6b.jpg", width = 9, height = 7)
