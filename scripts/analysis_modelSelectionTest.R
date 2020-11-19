9562d4a5c3c39d9543dff668eec35522bad1dd57
Helen Killeen <hjkilleen@ucdavis.edu>
2020-11-18 16:04:44 -0800

    add nonlinear temp term to model selection

--- scripts/analysis_modelSelectionTest.R
+++ scripts/analysis_modelSelectionTest.R
@@ -9,15 +9,15 @@ library(MuMIn)
 epc <- ep[complete.cases(ep),]#filter to only complete cases
 
 #Optimize random effects structure using maximum likelihood
-ep.int <- lmer(scale(length) ~ sex*temp_2_z + temp_100_z + sex:temp_100_z + sst_sd_z + sex:sst_sd_z + chla_z + sex:chla_z + sla_z + sex:sla_z + moci_spring_z + sex:moci_spring_z + cuti_z + sex:cuti_z + (1|station), data = epc, na.action = na.fail, REML = FALSE) #model with random intercept 
+ep.int <- lmer(scale(length) ~ sex*temp_2_z + I(temp_2_z^2) + temp_100_z + sex:temp_100_z + sst_sd_z + sex:sst_sd_z + chla_z + sex:chla_z + sla_z + sex:sla_z + moci_spring_z + sex:moci_spring_z + cuti_z + sex:cuti_z + (1|station), data = epc, na.action = na.fail, REML = FALSE) #model with random intercept 
 
-ep.intSlope <- lmer(scale(length) ~ sex*temp_2_z + temp_100_z + sex:temp_100_z + sst_sd_z + sex:sst_sd_z + chla_z + sex:chla_z + sla_z + sex:sla_z + moci_spring_z + sex:moci_spring_z + cuti_z + sex:cuti_z + (1+temp_2_z|station), data = epc, na.action = na.fail, REML = FALSE)#model with random intercept and slope
+ep.intSlope <- lmer(scale(length) ~ sex*temp_2_z + I(temp_2_z^2) + temp_100_z + sex:temp_100_z + sst_sd_z + sex:sst_sd_z + chla_z + sex:chla_z + sla_z + sex:sla_z + moci_spring_z + sex:moci_spring_z + cuti_z + sex:cuti_z + (1+temp_2_z|station), data = epc, na.action = na.fail, REML = FALSE)#model with random intercept and slope
 
 anova(ep.int, ep.intSlope)#compare models with different random effect structure
 #Slope intercept is the optimal random effects structure
 
 #Optimize fixed effect structure using AIC
-ep.global.model <- lmer(scale(length) ~ sex*temp_2_z + temp_100_z + sex:temp_100_z + sst_sd_z + sex:sst_sd_z + chla_z + sex:chla_z + sla_z + sex:sla_z + moci_spring_z + sex:moci_spring_z + cuti_z + sex:cuti_z + (1+temp_2_z|station), data = epc, na.action = na.fail, REML = FALSE)
+ep.global.model <- lmer(scale(length) ~ sex*temp_2_z + I(temp_2_z^2) + temp_100_z + sex:temp_100_z + sst_sd_z + sex:sst_sd_z + chla_z + sex:chla_z + sla_z + sex:sla_z + moci_spring_z + sex:moci_spring_z + cuti_z + sex:cuti_z + (1+temp_2_z|station), data = epc, na.action = na.fail, REML = FALSE)
 
 ep.model.set <- dredge(ep.global.model)
 
@@ -35,15 +35,15 @@ sjPlot::plot_model(ep.model)
 tsc <- ts[complete.cases(ts),]#filter to only complete cases
 
 #Optimize random effects structure using maximum likelihood
-ts.int <- lmer(scale(length) ~ sex*temp_2_z + temp_100_z + sex:temp_100_z + sst_sd_z + sex:sst_sd_z + chla_z + sex:chla_z + sla_z + sex:sla_z + moci_spring_z + sex:moci_spring_z + cuti_z + sex:cuti_z + (1|station), data = tsc, na.action = na.fail, REML = FALSE) #model with random intercept 
+ts.int <- lmer(scale(length) ~ sex*temp_2_z + I(temp_2_z^2) + temp_100_z + sex:temp_100_z + sst_sd_z + sex:sst_sd_z + chla_z + sex:chla_z + sla_z + sex:sla_z + moci_spring_z + sex:moci_spring_z + cuti_z + sex:cuti_z + (1|station), data = tsc, na.action = na.fail, REML = FALSE) #model with random intercept 
 
-ts.intSlope <- lmer(scale(length) ~ sex*temp_2_z + temp_100_z + sex:temp_100_z + sst_sd_z + sex:sst_sd_z + chla_z + sex:chla_z + sla_z + sex:sla_z + moci_spring_z + sex:moci_spring_z + cuti_z + sex:cuti_z + (1+temp_2_z|station), data = tsc, na.action = na.fail, REML = FALSE)#model with random intercept and slope
+ts.intSlope <- lmer(scale(length) ~ sex*temp_2_z + I(temp_2_z^2) + temp_100_z + sex:temp_100_z + sst_sd_z + sex:sst_sd_z + chla_z + sex:chla_z + sla_z + sex:sla_z + moci_spring_z + sex:moci_spring_z + cuti_z + sex:cuti_z + (1+temp_2_z|station), data = tsc, na.action = na.fail, REML = FALSE)#model with random intercept and slope
 
 anova(ts.int, ts.intSlope)#compare models with different random effect structure
 #Slope intercept is the optimal random effects structure
 
 #Optimize fixed effect structure using AIC
-ts.global.model <- lmer(scale(length) ~ sex*temp_2_z + temp_100_z + sex:temp_100_z + sst_sd_z + sex:sst_sd_z + chla_z + sex:chla_z + sla_z + sex:sla_z + moci_spring_z + sex:moci_spring_z + cuti_z + sex:cuti_z + (1+temp_2_z|station), data = tsc, na.action = na.fail, REML = FALSE)
+ts.global.model <- lmer(scale(length) ~ sex*temp_2_z + I(temp_2_z^2) + temp_100_z + sex:temp_100_z + sst_sd_z + sex:sst_sd_z + chla_z + sex:chla_z + sla_z + sex:sla_z + moci_spring_z + sex:moci_spring_z + cuti_z + sex:cuti_z + (1+temp_2_z|station), data = tsc, na.action = na.fail, REML = FALSE)
 
 ts.model.set <- dredge(ts.global.model)
 
@@ -53,8 +53,7 @@ ts.top.model
 
 
 #top model with full dataset with random slope and intercept for SST
-ts.model <- lmer(scale(length) ~ chla_z + sex + sla_z + sst_sd_z + temp_100_z +  
-                   temp_2_z + (1 + temp_2_z | station), data = ts)
+ts.model <- lmer(scale(length) ~ chla_z + cuti_z + moci_spring_z + sex + sla_z + sst_sd_z + temp_100_z + temp_2_z + (1 + temp_2_z | station) + chla_z:sex + sex:sla_z + sex:sst_sd_z + sex:temp_100_z + sex:temp_2_z, data = ts)
 lattice::dotplot(ranef(ts.model,condVar=TRUE))
 sjPlot::plot_model(ts.model)
 
@@ -62,15 +61,15 @@ sjPlot::plot_model(ts.model)
 ndc <- nd[complete.cases(nd),]#filter to only complete cases
 
 #Optimize random effects structure using maximum likelihood
-nd.int <- lmer(scale(length) ~ sex*temp_2_z + temp_100_z + sex:temp_100_z + sst_sd_z + sex:sst_sd_z + chla_z + sex:chla_z + sla_z + sex:sla_z + moci_spring_z + sex:moci_spring_z + cuti_z + sex:cuti_z + (1|station), data = ndc, na.action = na.fail, REML = FALSE) #model with random intercept 
+nd.int <- lmer(scale(length) ~ sex*temp_2_z + I(temp_2_z^2) + temp_100_z + sex:temp_100_z + sst_sd_z + sex:sst_sd_z + chla_z + sex:chla_z + sla_z + sex:sla_z + moci_spring_z + sex:moci_spring_z + cuti_z + sex:cuti_z + (1|station), data = ndc, na.action = na.fail, REML = FALSE) #model with random intercept 
 
-nd.intSlope <- lmer(scale(length) ~ sex*temp_2_z + temp_100_z + sex:temp_100_z + sst_sd_z + sex:sst_sd_z + chla_z + sex:chla_z + sla_z + sex:sla_z + moci_spring_z + sex:moci_spring_z + cuti_z + sex:cuti_z + (1+temp_2_z|station), data = ndc, na.action = na.fail, REML = FALSE)#model with random intercept and slope
+nd.intSlope <- lmer(scale(length) ~ sex*temp_2_z + I(temp_2_z^2) + temp_100_z + sex:temp_100_z + sst_sd_z + sex:sst_sd_z + chla_z + sex:chla_z + sla_z + sex:sla_z + moci_spring_z + sex:moci_spring_z + cuti_z + sex:cuti_z + (1+temp_2_z|station), data = ndc, na.action = na.fail, REML = FALSE)#model with random intercept and slope
 
 anova(nd.int, nd.intSlope)#compare models with different random effect structure
 #Slope intercept is the optimal random effects structure
 
 #Optimize fixed effect structure using AIC
-nd.global.model <- lmer(scale(length) ~ sex*temp_2_z + temp_100_z + sex:temp_100_z + sst_sd_z + sex:sst_sd_z + chla_z + sex:chla_z + sla_z + sex:sla_z + moci_spring_z + sex:moci_spring_z + cuti_z + sex:cuti_z + (1+temp_2_z|station), data = ndc, na.action = na.fail, REML = FALSE)
+nd.global.model <- lmer(scale(length) ~ sex*temp_2_z + I(temp_2_z^2) + temp_100_z + sex:temp_100_z + sst_sd_z + sex:sst_sd_z + chla_z + sex:chla_z + sla_z + sex:sla_z + moci_spring_z + sex:moci_spring_z + cuti_z + sex:cuti_z + (1+temp_2_z|station), data = ndc, na.action = na.fail, REML = FALSE)
 
 nd.model.set <- dredge(nd.global.model)
 
