ff69b553321c8f9be1ca55b9efa42897d777c7eb
Helen Killeen <hjkilleen@ucdavis.edu>
2020-12-03 10:57:23 -0800

    Function to calculate kappa for lmer

--- scripts/functions/length_frequency.R
+++ scripts/functions/length_frequency.R
@@ -105,3 +105,22 @@ get.cuti <- function(x, y, z) {
   cuti.val <- mean(filter(cuti, latitude.round == lat, year == y, date >= start.date, date <= end.date)$cuti)
   cuti.val
 }
+
+kappa.mer <- function (fit, scale = TRUE, center = FALSE, add.intercept = TRUE, #source: https://rdrr.io/github/jasongraf1/JGmermod/src/R/kappa.mer.R
+                       exact = FALSE) {
+  # adapted version of base R's kappa()
+  X = getME(fit,"X")
+  nam = names(fixef(fit))
+  ## exclude intercepts
+  nrp = sum(1 * (nam == "(Intercept)"))
+  if (nrp > 0) {
+    X = X[, -(1:nrp), drop = FALSE]
+    nam = nam[-(1:nrp)]
+  }
+  if (add.intercept) {
+    X = cbind(rep(1), scale(X, scale = scale, center = center))
+    kappa(X, exact = exact)
+  } else {
+    kappa(scale(X, scale = scale, center = center), exact = exact)
+  }
+}
