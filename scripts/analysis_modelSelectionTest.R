5a7f39395fee7f3b66c15fcdd9c064dfa57271e8
Helen Killeen <hjkilleen@ucdavis.edu>
2020-12-03 10:58:07 -0800

    tests for multicollinearity

--- scripts/analysis_modelSelectionTest.R
+++ scripts/analysis_modelSelectionTest.R
@@ -1,6 +1,7 @@
+library(lme4)
 library(arm)
 library(MuMIn)
-
+library(olsrr)
 # Tue Nov 17 17:10:11 2020 ------------------------------
 
 #Based on example in https://onlinelibrary.wiley.com/doi/full/10.1111/j.1420-9101.2010.02210.x
@@ -26,7 +27,8 @@ ep.top.model <- get.models(ep.model.set, subset = 1)
 ep.top.model
 
 #top model with full dataset with random slope and intercept for SST
-ep.model <- lmer(scale(length) ~ chla_z + moci_spring_z + sex + sla_z + sst_sd_z + temp_100_z + temp_2_z + (1 + temp_2_z | station) + moci_spring_z:sex + sex:sla_z + sex:temp_100_z + sex:temp_2_z, data = ep)
+ep.model <- lmer(scale(length) ~ chla_z + moci_spring_z + sex +  sst_sd_z + temp_100_z + temp_2_z + (1 + temp_2_z | station) + moci_spring_z:sex +  sex:temp_100_z + sex:temp_2_z, data = ep)
+car::vif(ep.model)#check for multicollinearilty using variance inflation factor <5 = ok
 lattice::dotplot(ranef(ep.model,condVar=TRUE))
 sjPlot::plot_model(ep.model)
 sjPlot::tab_model(ep.model, 
@@ -75,6 +77,7 @@ ts.top.model
 
 #top model with full dataset with random slope and intercept for SST
 ts.model <- lmer(scale(length) ~ chla_z + cuti_z + moci_spring_z + sex + sla_z + sst_sd_z + temp_2_z + (1 + temp_2_z | station) + chla_z:sex + sex:sla_z + sex:temp_2_z, data = ts)
+car::vif(ts.model)#check for multicollinearilty using variance inflation factor <5 = ok
 lattice::dotplot(ranef(ts.model,condVar=TRUE))
 sjPlot::plot_model(ts.model)
 sjPlot::tab_model(ts.model, 
@@ -86,14 +89,14 @@ sjPlot::tab_model(ts.model,
 ndc <- nd[complete.cases(nd),]#filter to only complete cases
 
 #Optimize random effects structure using maximum likelihood
-nd.int <- lmer(scale(length) ~ sex*temp_2_z + temp_100_z + sex:temp_100_z + sst_sd_z + sex:sst_sd_z + chla_z + sex:chla_z + sla_z + sex:sla_z + moci_spring_z + sex:moci_spring_z + cuti_z + sex:cuti_z + (1|station), data = ndc, na.action = na.fail, REML = FALSE) #model with random intercept 
+nd.int <- lmer(scale(length) ~ sex*temp_2_z + temp_100_z + sex:temp_100_z + sst_sd_z + sex:sst_sd_z + chla_z + sex:chla_z +  moci_spring_z + sex:moci_spring_z + cuti_z + sex:cuti_z + (1|station), data = ndc, na.action = na.fail, REML = FALSE) #model with random intercept 
 
-nd.intSlope <- lmer(scale(length) ~ sex*temp_2_z + temp_100_z + sex:temp_100_z + sst_sd_z + sex:sst_sd_z + chla_z + sex:chla_z + sla_z + sex:sla_z + moci_spring_z + sex:moci_spring_z + cuti_z + sex:cuti_z + (1+temp_2_z|station), data = ndc, na.action = na.fail, REML = FALSE)#model with random intercept and slope
+nd.intSlope <- lmer(scale(length) ~ sex*temp_2_z + temp_100_z + sex:temp_100_z + sst_sd_z + sex:sst_sd_z + chla_z + sex:chla_z + moci_spring_z + sex:moci_spring_z + cuti_z + sex:cuti_z + (1+temp_2_z|station), data = ndc, na.action = na.fail, REML = FALSE)#model with random intercept and slope
 
 #Random slope and intercept model is singular fit. Use random intercept model as global model. 
 
 #Optimize fixed effect structure using AIC
-nd.global.model <- lmer(scale(length) ~ sex*temp_2_z + temp_100_z + sex:temp_100_z + sst_sd_z + sex:sst_sd_z + chla_z + sex:chla_z + sla_z + sex:sla_z + moci_spring_z + sex:moci_spring_z + cuti_z + sex:cuti_z + (1|station), data = ndc, na.action = na.fail, REML = FALSE)
+nd.global.model <- lmer(scale(length) ~ sex*temp_2_z + temp_100_z + sex:temp_100_z + sst_sd_z + sex:sst_sd_z + chla_z + sex:chla_z + moci_spring_z + sex:moci_spring_z + sla_z + sex:sla_z + cuti_z + sex:cuti_z + (1|station), data = ndc, na.action = na.fail, REML = FALSE)
 
 nd.model.set <- dredge(nd.global.model)
 
@@ -103,7 +106,12 @@ nd.top.model
 
 
 #top model with full dataset with random slope and intercept for SST
-nd.model <- lmer(scale(length) ~ chla_z + cuti_z + sex + sla_z + sst_sd_z + temp_100_z + temp_2_z + (1 | station) + cuti_z:sex, data = nd)
+nd.model <- lmer(scale(length) ~ chla_z + cuti_z + moci_spring_z + sex + temp_100_z + temp_2_z + (1 | station) + cuti_z:sex + moci_spring_z:sex, data = nd)
+car::vif(nd.global.model)#check for multicollinearilty using variance inflation factor <5 = ok
+kappa.mer(nd.global.model) #check condition index for model (mixed effect variation)
+#kappa < 10 is reasonable collinearity,
+# kappa < 30 is moderate collinearity,
+# kappa >= 30 is troubling collinearity
 lattice::dotplot(ranef(nd.model,condVar=TRUE))
 sjPlot::plot_model(nd.model)
 sjPlot::tab_model(nd.model, 
