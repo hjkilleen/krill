ce0fd04a1c6f13e33d03ee78dbd3275e9f9b2f71
Helen Killeen <hjkilleen@ucdavis.edu>
2020-12-14 18:23:46 -0800

    new script to merge environmental and length data

---
+++ scripts/data_merge.R
@@ -0,0 +1,8 @@
+#Merge krill lengths and environmental data into one dataset
+# Fri Dec 11 16:15:42 2020 ------------------------------
+
+#LIBRARIES & SOURCES
+#====
+source("scripts/data_load.R")
+#====
+
