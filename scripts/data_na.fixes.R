ed76ac47e061a6ef474c20ffe987d0eb2c0fcb6b
Helen Killeen <hjkilleen@ucdavis.edu>
2021-01-27 11:38:30 -0800

    missing chlorophyll data and subsurface documentation

--- scripts/data_na.fixes.R
+++ scripts/data_na.fixes.R
@@ -38,34 +38,16 @@ View(x)
 #====
 #118, 421, 166
 #load legacy ROMS data using positions provided by Keith
+#=====
 
-#110(2012), 454(2011)  missing date and chla
-load("data/allLengths.rda")
-#110(2012) = 2012-05-08
-View(filter(allLengthsEnv, station == 454, year ==2011))
-#drop haul 8, plenty of measured krill from haul 9
-#454(2011) = 2011-05-04
-#drop these (2) because they are apparently from haul 19, whereas all others are from haul 6
-#problem solved by rerunning code
-#fixed
+#MISSING SUBSURFACE TEMP
+#====
+#There were three stations with depth>100m that had missing subsurface temp values, 422, 421, and 453. All three were in areas of deep nearshore shelf, too close to shore for ROMS resolution. 
+#====
 
-#156(2012), 483(2012) missing date, chla, and temp_2/sst_sd
-#problem solved by rerunning code...
-#fixed
-
-#453(2011, 2015-2017) missing temp_2 and sst_sd
-View(filter(waterTemp, station == 453))
-#might not be downloading data from server properly? tested this with virtual sensor URL
-#fixed
-
-#482(2018) missing chla
-View(filter(allLengths, station == 482))
-#looks like this data wasn't acquired, ask Chelle, date = 2018-05-08
-# Fri Nov  6 14:38:39 2020 ------------------------------
-
-#165(2012) chla is Nan
-View(filter(env, station == 165))
-#data was unavailable in the dataset
-#no action required. 
+#MISSING CHLOROPHYLL
+#====
+#114, 167, 421, 170, 165 all had missing chla values in various years. This was because no data was available within 13 days of sampling. 
+#====
 
 
