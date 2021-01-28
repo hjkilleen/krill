de4a60d47eca9c3a3fc1d165be49c50b3b0f6385
Helen Killeen <hjkilleen@ucdavis.edu>
2021-01-28 09:33:40 -0800

    subfigure c

--- scripts/figures/figure3_interannualModel.R
+++ scripts/figures/figure3_interannualModel.R
@@ -26,7 +26,7 @@ load("data/interannualCoefficients.rda")
 
 #SUBFIGURE B - PLOT
 #====
-pmsum <- summarize(group_by(pmsimsum, year), sim.mean = mean(sim.mean), lower.95 = mean(lower.95), upper.95 = mean(upper.95))
+pmsum <- summarize(group_by_at(pmsimsum, vars(year, sex)), sim.mean = mean(sim.mean), lower.95 = mean(lower.95), upper.95 = mean(upper.95))
 epsum <- summarize(group_by_at(epsimsum, vars(year, sex)), sim.mean = mean(sim.mean), lower.95 = mean(lower.95), upper.95 = mean(upper.95))
 tssum <- summarize(group_by_at(tssimsum, vars(year, sex)), sim.mean = mean(sim.mean), lower.95 = mean(lower.95), upper.95 = mean(upper.95))
 ndsum <- summarize(group_by_at(ndsimsum, vars(year, sex)), sim.mean = mean(sim.mean), lower.95 = mean(lower.95), upper.95 = mean(upper.95))
@@ -45,7 +45,38 @@ b <- ggplot() +
 
 #SUBFIGURE C - SEXUAL DIMORPHISM
 #====
+pdiffs <- dcast(pmsum, year~sex, value.var = "sim.mean", mean)#gather differences between simulated male and female lengths 
+pdiffs$diff <- pdiffs$F - pdiffs$M
+pdiffs$prop <- pdiffs$diff/mean(pdiffs$F)
+pdiffs$species <- rep("P", nrow(pdiffs))
 
+epdiffs <- dcast(epsum, year~sex, value.var = "sim.mean", mean)
+epdiffs$diff <- epdiffs$F - epdiffs$M
+epdiffs$prop <- epdiffs$diff/mean(epdiffs$F)
+epdiffs$species <- rep("EP", nrow(epdiffs))
+
+tsdiffs <- dcast(tssum, year~sex, value.var = "sim.mean", mean)
+tsdiffs$diff <- tsdiffs$F - tsdiffs$M
+tsdiffs$prop <- tsdiffs$diff/mean(tsdiffs$F)
+tsdiffs$species <- rep("TS", nrow(tsdiffs))
+
+nddiffs <- dcast(ndsum, year~sex, value.var = "sim.mean", mean)
+nddiffs$diff <- nddiffs$F - nddiffs$M
+nddiffs$prop <- nddiffs$diff/mean(nddiffs$F)
+nddiffs$species <- rep("ND", nrow(nddiffs))
+
+diffs <- rbind(epdiffs, tsdiffs)#bind species and pooled differences
+diffs <- rbind(diffs, nddiffs)
+diffs <- rbind(diffs, pdiffs)
+
+x <- summarize(group_by_at(diffs, vars(year, species)), mean = mean(prop))
+
+c <- ggplot(x) + 
+  geom_line(aes(x = year, y = mean, group = species, color = species), size = 2) + 
+  geom_hline(yintercept = 0) + 
+  scale_color_manual(name = "Species", values = c("#E69F00", "#009E73", "grey", "#56B4E9"), labels = c("E. pacifica", "N. difficilis","Pooled", "T. spinifera")) + 
+  labs(x = "Year", y = "Size difference F - M (% of mean length)") +
+  theme(text = element_text(size = 20), legend.position = "none")
 #====
 
 #MERGE FIGURES
