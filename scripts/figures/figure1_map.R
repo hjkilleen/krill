6f970cd7890ba18df2001113687fadc683a46414
Helen Killeen <hjkilleen@ucdavis.edu>
2021-02-02 10:39:01 -0800

    add regional boundaries and labels to map

--- scripts/figures/figure1_map.R
+++ scripts/figures/figure1_map.R
@@ -14,14 +14,16 @@ library(sp)
 library(maps)
 library(geosphere)
 library(cowplot)
+load("data/metadata.rda")
 #====
 
 #SETUP
 #====
-#General mapping objects
-world <- ne_countries(scale = "medium", returnclass = "sf")
+regions <- read_csv("data/regions.csv")#add regions for merging shore identifiers
 
-states <- sf::st_as_sf(map("state", plot = FALSE, fill = TRUE))
+boundaries <- data.frame(x1 = c(-125.5, -125.5, -125.5), y1 = c(34.448, 36.306, 37.948), x2 = c(-120.472, -121.901, -122.785), y2 = c(34.448, 36.306, 37.948))#create gridpoints for regional boundaries
+
+states <- sf::st_as_sf(map("state", plot = FALSE, fill = TRUE))#state lines and coast
 CAfromstates <- states %>%
   subset(., states$ID == "california")
 
@@ -49,12 +51,18 @@ map <- ggplot(data = states) +
     style = north_arrow_fancy_orienteering
   ) +
   scale_shape_discrete(labels = c("Offshore", "Onshore")) +
+  geom_segment(data = boundaries, aes(x = x1, y = y1, xend = x2, yend = y2)) + 
+  annotate("text", x = -124.75, y = 33.75, label = "South", size = 5) + 
+  annotate("text", x = -124.75, y = 35.25, label = "Central", size = 5) + 
+  annotate("text", x = -124.75, y = 37, label = "North\nCentral", size = 5) + 
+  annotate("text", x = -124.75, y = 38.75, label = "North", size = 5) +
   theme(text = element_text(size = 25)) +
   theme(axis.title.x = element_blank(),
         axis.title.y = element_blank(),
         legend.position = c(0.77, 0.6),
         legend.title = element_blank(),
-        legend.background = element_rect(fill = "NA"))
+        legend.background = element_rect(fill = "NA")) + 
+  guides(shape = guide_legend(override.aes = list(size = 5)))
 
 #Inset Map
 inset <- ggplot(data = states) +
