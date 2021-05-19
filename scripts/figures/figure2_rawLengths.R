04c4d43868117349b9c943d25f27b87da9bbaef0
Helen Killeen <hjkilleen@ucdavis.edu>
2021-05-19 11:36:52 -0700

    modifications to fig 2

--- scripts/figures/figure2_rawLengths.R
+++ scripts/figures/figure2_rawLengths.R
@@ -1,10 +1,11 @@
 #Figure 2
 #Krill raw length frequencies
+# Wed Feb 17 09:23:04 2021 ------------------------------
 
-# Wed Jan 27 15:52:46 2021 ------------------------------
 #LIBRARIES & SOURCES
 #====
 library(ggpubr)
+library(tidyverse)
 load("data/allLengthsEnv.rda")
 source("scripts/functions/length_frequency.R")
 #====
@@ -13,6 +14,8 @@ source("scripts/functions/length_frequency.R")
 #====
 #Histogram of krill lengths by species
 aLE_tally <- add_tally(group_by_at(allLengthsEnv, vars(year, species)))
+aLE_tally$species = factor(aLE_tally$species, levels=c("EP", "TS", "ND"))
+allLengthsEnv$species = factor(allLengthsEnv$species, levels=c("EP", "TS", "ND"))
 labs <- c("E. pacifica", "T. spinifera", "N. difficilis")
 names(labs) <- c("EP", "TS", "ND")
 a <- ggplot(filter(allLengthsEnv), aes(x = as.factor(year), y = length.unscaled, group = year, fill = as.factor(year))) +
@@ -21,8 +24,9 @@ a <- ggplot(filter(allLengthsEnv), aes(x = as.factor(year), y = length.unscaled,
   scale_fill_manual(values = c("#ffffff80", "#00000080", "#ffffff80", "#00000080", "#ffffff80", "#00000080", "#ffffff80"), labels = c("2011", "2012", "2013", "2015", "2016", "2017", "2018")) +
   facet_wrap(.~species, nrow = 3, ncol = 1, labeller = labeller(species = labs)) + 
   geom_text(data = summarize(group_by_at(aLE_tally, vars(species, year)), n=mean(nnn), max = max(length.unscaled)), aes(x = as.factor(year), y = max + 5, label = paste("n=", n, sep = " ")), color = "black", size = 4) +
-  geom_text(data = summarize(group_by_at(aLE_tally, vars(year, species)), mean = round(mean(length.unscaled), 1), max = max(length.unscaled)), aes(x = as.factor(year), y = max + 10, label = paste("mean=", mean, sep = " ")), color = "black", size = 4) +
+  geom_text(data = summarize(group_by_at(aLE_tally, vars(year, species)), mean = round(mean(length.unscaled), 1), max = max(length.unscaled)), aes(x = as.factor(year), y = max + 13, label = paste("mean=", mean, sep = " ")), color = "black", size = 4) +
   labs(x = "Year", y = "Length (mm)") + 
+  theme_bw() +
   theme(text = element_text(size = 20), legend.position = "none")
 #====
 
@@ -32,16 +36,18 @@ a <- ggplot(filter(allLengthsEnv), aes(x = as.factor(year), y = length.unscaled,
 #=====
 x <- as.data.frame(summarize(group_by_at(allLengthsEnv, vars(year, species)), cv = cv(length.unscaled)))
 x$year <- as.factor(x$year)
-x$species <- as.factor(x$species)
+x$species = factor(x$species, levels=c("EP", "TS", "ND"))
 b <- ggplot(x, aes(x = year, y = cv, color = species, group = species)) +
-  geom_point() + 
+  geom_point(size = 2) + 
   geom_line() + 
-  scale_color_manual(values = c("#E69F00", "#009E73", "#56B4E9"), labels = c("E. pacifica", "N. difficilis", "T. spinifera")) +
+  scale_color_manual(values = c("#E69F00", "#56B4E9", "#009E73"), labels = c("E. pacifica", "T. spinifera", "N. difficilis")) +
   labs(x = "Year", y = "Coefficient\nof Variation", color = "Species") +
-  theme(text = element_text(size = 15), legend.position = "bottom")
+  theme_classic() +
+  theme(text = element_text(size = 20), legend.position = "bottom")
 #====
 
 #MERGED FIGURE
 #====
 ggarrange(a, b, labels = c("A", "B"), ncol = 1, nrow = 2, heights = c(10, 4), align = "v", label.x = .05)
+ggsave("figures/manuscript/figure2_krillViolinCV.jpeg", width = 8, height = 11, dpi = 300)
 #====
