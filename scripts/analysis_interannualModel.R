418ba58f4333fd4654c1552e5e330cba9f93faba
Helen Killeen <hjkilleen@ucdavis.edu>
2021-05-19 11:34:45 -0700

    general supplemental tables

--- scripts/analysis_interannualModel.R
+++ scripts/analysis_interannualModel.R
@@ -40,6 +40,26 @@ ndc.noSex <- data.frame(predictor = attr(fixef(ndm.noSex), "names"),
 interannualCoefficients.noSex <- list(pmc.noSex, epc.noSex, tsc.noSex, ndc.noSex)#merge as list
 save(interannualCoefficients.noSex, file = "output/interannualCoefficients_noSex.rda")#save list
 
+#Generate output supplementary tables
+tab_model(epm.noSex, title = "E. pacifica (year only)", show.p = FALSE, CSS = list(
+  css.depvarhead = '+color: red;',
+  css.centeralign = 'text-align: center;', 
+  css.firsttablecol = 'font-weight: bold;', 
+  css.summary = 'color: blue;'
+))
+tab_model(tsm.noSex, title = "T. spinifera (year only)", show.p = FALSE, CSS = list(
+  css.depvarhead = '+color: red;',
+  css.centeralign = 'text-align: center;', 
+  css.firsttablecol = 'font-weight: bold;', 
+  css.summary = 'color: blue;'
+))
+tab_model(ndm.noSex, title = "N. difficilis (year only)", show.p = FALSE, CSS = list(
+  css.depvarhead = '+color: red;',
+  css.centeralign = 'text-align: center;', 
+  css.firsttablecol = 'font-weight: bold;', 
+  css.summary = 'color: blue;'
+))
+
 #Drop 2011-2013 N. difficilis due to limited sampling
 allLengthsEnv <- allLengthsEnv[!(allLengthsEnv$species == "ND" & allLengthsEnv$year %in% c("2011", "2012", "2013")),]
 nd <- nd[!(nd$year %in% c("2011", "2012", "2013")),]
@@ -59,6 +79,26 @@ ndc <- data.frame(predictor = attr(fixef(ndm), "names"),
                   coefficient = as.vector(fixef(ndm)))
 interannualCoefficients <- list(pmc, epc, tsc, ndc)#merge as list
 save(interannualCoefficients, file = "output/interannualCoefficients.rda")#save list
+
+#Generate output tables
+tab_model(epm, title = "E. pacifica (year and sex)", show.p = FALSE, CSS = list(
+  css.depvarhead = '+color: red;',
+  css.centeralign = 'text-align: center;', 
+  css.firsttablecol = 'font-weight: bold;', 
+  css.summary = 'color: blue;'
+))
+tab_model(tsm, title = "T. spinifera (year and sex)", show.p = FALSE, CSS = list(
+  css.depvarhead = '+color: red;',
+  css.centeralign = 'text-align: center;', 
+  css.firsttablecol = 'font-weight: bold;', 
+  css.summary = 'color: blue;'
+))
+tab_model(ndm, title = "N. difficilis (year and sex)", show.p = FALSE, CSS = list(
+  css.depvarhead = '+color: red;',
+  css.centeralign = 'text-align: center;', 
+  css.firsttablecol = 'font-weight: bold;', 
+  css.summary = 'color: blue;'
+))
 #====
 
 #SIMULATION
