257fa447ac93aeb687d02e57bd543ba351d98819
Helen Killeen <hjkilleen@ucdavis.edu>
2021-06-17 14:42:58 -0700

    change table 2 regional delineations to match NMFS RREAS

--- scripts/figures/table2_dataSummary.R
+++ scripts/figures/table2_dataSummary.R
@@ -30,9 +30,22 @@ grouped2 <- grouped[,col_order]
 
 grouped2[is.na(grouped2)] <- 0
 
+grouped2$EP.F.core <- grouped2$EP.F.north+grouped2$EP.F.north_central#combine north and north central into a single "core" region to match NMFW RREAS geography
+grouped2$EP.M.core <- grouped2$EP.M.north+grouped2$EP.M.north_central
+grouped2$TS.F.core <- grouped2$TS.F.north+grouped2$TS.F.north_central
+grouped2$TS.M.core <- grouped2$TS.M.north+grouped2$TS.M.north_central
+grouped2$ND.F.core <- grouped2$ND.F.north+grouped2$ND.F.north_central
+grouped2$ND.M.core <- grouped2$ND.M.north+grouped2$ND.M.north_central
+
+grouped2 <- grouped2[,-c(2, 3, 6, 7, 10, 11, 14, 15, 18, 19, 22, 23)]#get rid of north and north_central regions
+
+col_order <- c("year", "EP.F.core", "EP.F.central", "EP.F.south", "EP.M.core", "EP.M.central", "EP.M.south", "TS.F.core", "TS.F.central", "TS.F.south", "TS.M.core", "TS.M.central", "TS.M.south", "ND.F.core", "ND.F.central", "ND.F.south", "ND.M.core", "ND.M.central", "ND.M.south")
+
+grouped2 <- grouped2[,col_order]
+
 grouped2 <- as.data.frame(adorn_totals(grouped2, "row"))
 
-names(grouped2) <- c("year", "N", "NC", "C", "S", "N", "NC", "C", "S", "N", "NC", "C", "S", "N", "NC", "C", "S", "N", "NC", "C", "S", "N", "NC", "C", "S")
+names(grouped2) <- c("year", "C", "SC", "S", "C", "SC", "S", "C", "SC", "S", "C", "SC", "S", "C", "SC", "S", "C", "SC", "S")
 #====
 
 #TABLE
@@ -40,8 +53,8 @@ names(grouped2) <- c("year", "N", "NC", "C", "S", "N", "NC", "C", "S", "N", "NC"
 grouped2 %>% 
   kbl() %>% 
   kable_classic() %>% 
-  add_header_above(c(" " = 1, "Female" = 4, "Male" = 4, "Female" = 4, "Male" = 4,"Female" = 4, "Male" = 4)) %>% 
-  add_header_above(c(" " = 1, "Euphausia pacifica" = 8, "Thysanoessa spinifera" = 8, "Nematocelis difficilis" = 8)) %>% 
+  add_header_above(c(" " = 1, "Female" = 3, "Male" = 3, "Female" = 3, "Male" = 3,"Female" = 3, "Male" = 3)) %>% 
+  add_header_above(c(" " = 1, "Euphausia pacifica" = 6, "Thysanoessa spinifera" = 6, "Nematocelis difficilis" = 6)) %>% 
   row_spec(8, bold = TRUE) %>% 
-  column_spec(c(1, 5, 9, 13, 17, 21, 25), border_right = TRUE)
+  column_spec(c(1, 4, 7, 10, 13, 16, 19), border_right = TRUE)
 #====
